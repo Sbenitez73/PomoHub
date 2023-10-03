cc7a4f07f63e7dd9dface3801c0c52564fb1ee58
Santiago Benitez <santibenitez2373@gmail.com>
2023-10-03 14:09:38 -0500

    Initial Commit

---
+++ Pomohub/PomohubApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomohubApp.swift
+//  Pomohub
+//
+//  Created by Santiago Benitez on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PomohubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
